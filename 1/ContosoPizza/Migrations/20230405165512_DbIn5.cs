2c430b06aaa9fb0b984787d3a02fe058ca448c75
Alexisqr <75916999+Alexisqr@users.noreply.github.com>
2023-04-07 17:53:45 +0300

    Add files via upload

---
+++ 1/ContosoPizza/Migrations/20230405165512_DbIn5.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ContosoPizza.Migrations
+{
+    /// <inheritdoc />
+    public partial class DbIn5 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "Calories",
+                table: "Toppings",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: 0m);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Calories",
+                table: "Toppings");
+        }
+    }
+}
